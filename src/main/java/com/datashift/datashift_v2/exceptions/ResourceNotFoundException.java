9e10e27325eea1c41370ab031b99bff7cd8e1741
MarioOnor <101718964+MarioOnor@users.noreply.github.com>
2025-07-16 14:22:18 +0800

    Completed the development for GET api

---
+++ src/main/java/com/datashift/datashift_v2/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.datashift.datashift_v2.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
